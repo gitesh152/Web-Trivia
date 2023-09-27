10efa1b9f03aa2643c7f2f7d34f7e60c12169af9
gitesh152 <gitesh152@gmail.com>
2023-09-27 23:42:24 +0530

    Trivia UI Developed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // Replace with your backend API's URL
+      // '/api': 'http://localhost:5000/api', // Replace with your backend API's URL
+      '/api': 'https://web-trivia.onrender.com/api', // Replace with your backend API's URL
     }
   },
   plugins: [react()],
